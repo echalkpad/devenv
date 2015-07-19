eb263d248bd3c0b97095bd5f1e7ac0c58d6b5923
JI-HUN KIM <jihuun.k@gmail.com>
2015-07-20 00:09:05 +0900

    Update hotdev 1507
    Signed-off-by: JI-HUN KIM <jihuun.k@gmail.com>

---
+++ hotdev/contents/150713.h
@@ -0,0 +1,87 @@
+<#지난주_핫_IT_프로덕트>
+
+1. 새로 출시된 고프로 카메라 GoPro Session
+보다 콤팩트하고 저렴함.
+http://9to5toys.com/2015/07/06/gopro-hero4-session-release/
+
+2. 영수증 프린터기를 이용해 스마트폰의 여러 컨텐츠를 출력. (사진,웹페이지,TODO리스트 등등을 간편하게 출력)
+http://www.earlyadopter.co.kr/59613
+
+3. 휴대용 태양광 발전기 Kalipak
+https://www.youtube.com/watch?v=gQp_2BoN2Cc
+
+4. 나무나 종이 표면을 태워서 글자를 새길 수 있는 돋보기
+http://www.earlyadopter.co.kr/59640
+
+5. 어디에도 설치가 가능한 방수 블루투스 스피커 (꼬리로 감아서 설치)
+http://www.businessinsider.com/boom-swimmer-waterproof-wireless-bluetooth-speaker-2015-7?utm_content=buffer969a4&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer
+
+6. 세계 곳곳에 인터넷을 보급할 수 있는 저궤도 위성.
+http://www.ipnomics.co.kr/?p=17261
+
+7. 가족과 소통하는 가정용 로봇.
+https://www.indiegogo.com/projects/buddy-your-family-s-companion-robot#/story
+
+8. 스키용 에어백, 눈속에 파뭍히지 않게 해주는 용도.
+http://gizmodo.com/mass-recall-of-avalanche-rescuing-airbags-thanks-to-st-1716916800?utm_campaign=socialflow_gizmodo_facebook&utm_source=gizmodo_facebook&utm_medium=socialflow
+
+9. 프로그래밍 가능한 로봇 장난감.
+http://www.engadget.com/2015/07/09/vortex-robot-toy-hands-on/
+
+10. 라떼아트를 출력해주는 커피 프린터기
+http://www.earlyadopter.co.kr/59498
+
+11. 소녀들을 위한 스마트 팔찌
+http://www.earlyadopter.co.kr/59597
+
+12. 벽을 기어 올라갈수 있는 장치
+http://interestingengineering.com/spider-style-climbing/
+
+13. 가정 내 수신 감도가 약한 Wi-Fi 지점을 위한 유선 연결 장치.
+Chromecast를 라우터에 직접 연결하여 Chromecast의 연결 속도와 안정성확보.
+https://store.google.com/product/_ethernet_adapter_for_chromecast
+
+14. 입는 옷에서 운동 효과를 파악할 수 있는 웨어러블 기기
+http://www.earlyadopter.co.kr/59525
+
+15. 결제가 가능한 손목밴드
+http://thenextweb.com/gadgets/2015/07/07/barclays-bpay-band-convenient-contactless-payments-if-youve-got-the-wrist-space/
+
+16. 뒤집어 끼워도 꽂아지는 마이크로 USB단자
+http://www.earlyadopter.co.kr/59523
+
+17. 얇고 가벼운 태양광 충전기
+http://www.earlyadopter.co.kr/59506
+
+18. 셀카 마이크, 인터뷰할때 누가 안찍어줘도 됨
+https://www.indiegogo.com/projects/solocam#/story
+
+19. 신용카드 크기의 어린이용 장난감 컴퓨터
+http://techcrunch.com/2015/07/07/the-microbit-is-shaping-up-to-be-the-perfect-programming-device-for-kids/?ncid=rss&utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+Techcrunch+%28TechCrunch%29
+
+
+20. 스마트폰 화면을 HUD(Head Up Display) 방식으로 볼수 있는 기기
+http://www.earlyadopter.co.kr/59395
+
+21. DIY 타투 기기
+http://www.earlyadopter.co.kr/59421
+
+22. 스트레스 관리 웨어러블 밴드 Wellbe
+https://www.youtube.com/watch?v=p_eDdX_to4I
+
+23. 빛을 프린트 하는 3D프린터기
+http://www.engadget.com/2015/07/05/painting-light-with-3d-tech/
+
+24. 총을 조준할때 고정되도록 도와주는 엑소 슈트
+http://www.digitaltrends.com/cool-tech/the-army-is-developing-an-arm-mounted-exoskeleton-that-helps-soldiers-aim/
+
+25. 노트북용 보조모니터
+http://www.geek.com/news/triple-your-laptop-display-space-with-slidenjoy-1627138/
+
+26. 패션에 좀 더 신경쓴 스마트 팔찌
+https://www.indiegogo.com/projects/helen-smart-bracelet-with-custom-straps#/story
+
+#kickstarter
+
+150713 <tech>
+세일러묵 드림.
---
+++ hotdev/contents/150720.h
@@ -0,0 +1,83 @@
+<#지난주_핫_IT_프로덕트>
+지난주 핫 했던 제품이 요약되어 있습니다.
+
+1. 기존의 스마트폰에 자석커넥터로 만들어주는 커넥터 단자.
+맥의 MacSafe 자석 커넥터를 폰에서 쓸 수 있게 해줌.
+https://www.kickstarter.com/projects/1041610927/znaps-the-9-magnetic-adapter-for-your-mobile-devic?ref=category_popular
+
+2. 익사 방지용 손목에 차는 비상용 에어 백
+물에 빠졌을때 이산화탄소 가스가 방출되어 바로 물에 뜨는 에어백을 터트릴 수 있음.
+https://www.indiegogo.com/projects/kingii-the-new-standard-in-water-safety#/story
+
+3. 모든 방향을 가공할 수 있는 5축CNC 밀링 머신.
+기존의 CNC밀링 머신은 한 평면만 가공이 가능.
+https://www.kickstarter.com/projects/1090944145/pocket-nc-the-first-5-axis-cnc-mill-for-your-deskt?ref=category
+
+4. 손동작으로 스마트와치나 스마트폰을 컨트롤 할 수 있는 블루투스 장치
+https://www.kickstarter.com/projects/belfio/deus-ex-aria-the-evolution-of-smartwatch-control
+
+5. 전자 기타 앰프로 유명한 Marshall에서 만든 스마트폰
+헤드폰 출력단자가 2개이며, 볼륨조절 버튼이 다이얼 방식
+http://www.theverge.com/2015/7/16/8976561/marshall-london-smartphone-images 
+
+6. 샤오미가 출시한 정수기
+http://thegear.co.kr/8828
+
+7. 샤오미가 출시한 스마트 신발
+http://techg.kr/6484
+
+8. 카메라를 스트랩이 아닌 몸에 바로 고정시킬 수 있는 홀더.
+https://peakdesign.com/store/capturepro
+
+9. 음주측정 스마트폰 악세서리
+http://www.earlyadopter.co.kr/59830
+
+10. 양변기에 부착해 물을 자동으로 내려주는 기기
+http://www.earlyadopter.co.kr/60224
+
+11. 작고 가볍고 접으면 휴대성도 좋은 전기 자전거
+http://www.earlyadopter.co.kr/60194
+
+12. 분실 방지 기능이 있는 스마트 우산.
+http://www.earlyadopter.co.kr/60068
+
+13. 촛불로 스마트폰을 충전할 수 있는 발전기
+http://www.earlyadopter.co.kr/60024
+
+14. 스마트폰을 클래식한 비디오 카메라로 바꿔주는 악세서리
+http://www.earlyadopter.co.kr/59910
+
+15. 피트니스 트래킹 전자발찌
+http://www.earlyadopter.co.kr/59887
+
+16. 자전거 휠을 디스플레이로 바꿔주는 조명.
+http://www.earlyadopter.co.kr/59886
+
+17. 고양이의 수분섭취량을 분석하는 기기.
+http://besuccess.com/2015/07/pura_indiegogo/
+
+18. 내장 이어폰이 있는 스마트폰 케이스
+http://www.earlyadopter.co.kr/60145
+
+19. 수험생등을 위한 단순한 스마트폰.
+http://www.earlyadopter.co.kr/60125
+
+20. 발사이즈가 커지면 크기를 늘릴 수 있는 신발.
+https://www.youtube.com/watch?v=GvMQLYTdYUA
+
+21. 증강현실 스마트 안경
+http://www.arm.com/innovation/products/air-3d-smart-glasses.php?sf39360701=1
+
+22. 스타벅스에 대기줄이 있는지 미리 알려주는 기기
+http://thenextweb.com/insider/2015/07/13/this-gadget-will-tell-you-if-theres-a-line-at-starbucks-before-you-arrive/
+
+23. 집 방문자가 총이 있는지 알려주는 기기
+http://www.engadget.com/2015/07/15/gundetect/
+
+24. 사람과 상호작용할 수 있는 로봇장난감.
+http://toyland.gizmodo.com/someone-at-pixar-built-the-most-adorable-peek-a-boo-pla-1717046545
+
+#kickstarter
+
+150720 <tech>
+세일러묵 드림.
