a5471686fff83eb5dbb131fec4004483ec7107f3
JI-HUN KIM <jihuun.k@gmail.com>
2015-08-10 01:04:01 +0900

    Add contents
    Signed-off-by: JI-HUN KIM <jihuun.k@gmail.com>

---
+++ hotdev/contents/150810.h
@@ -0,0 +1,79 @@
+<#지난주_핫_IT_프로덕트>
+지난주 핫 했던 제품이 요약되어 있습니다.
+
+1. 테슬라용 무인 충전기. 뱀모양의 로봇팔이 스스로 충전단자에 연결됨.
+http://techneedle.com/archives/21592?utm_source=dlvr.it&utm_medium=facebook
+
+2. 지형의 제약에서 자유로워지는 톡특한 메커니즘의 자동차
+http://www.digitaltrends.com/cars/swincar-spider-electric-car-pictures-specs-video-performance/#/4
+
+3. 서서 타고다니는 이동수단. 
+https://www.youtube.com/watch?v=XvG_356itPs
+
+4. 차량용 공기청정기.
+http://www.earlyadopter.co.kr/61578
+
+5. 아침 햇빛 처럼 불빛 으로도 깨워주는 알람시계 
+http://www.techinsider.io/i-tried-the-famous-wake-up-light-for-a-week-2015-8?utm_content=buffer54323&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer
+
+6. 고령자용 웨어러블 엑소슈트. (현대자동차)
+http://m.etnews.com/20150806000237
+
+7. 스탠포드대, 차세대 가상현실 기기 공개
+투명 OLED를 이용 VR기기 사용시의 멀미를 줄여주는 기술.
+http://www.bloter.net/archives/235437
+
+8. 로봇이 동작을 똑같이 따라하게 만드는 엑소슈트
+https://www.youtube.com/watch?v=A54CBfRBTYU
+
+9. 원하는 사운드대로 조립하는 모듈형 스피커. 
+크고작은 스피커의 조합으로 원하는 사운드의 스피커 구축.
+http://www.engadget.com/2015/08/02/sparkblocks-modular-speaker/
+
+10. 콘센트 플러그만한 애플워치 충전 거치대
+http://www.earlyadopter.co.kr/61644
+
+11. 음주 측정을 해야 풀 수 있는 자전거 자물쇠
+http://www.earlyadopter.co.kr/61663
+
+12. 유리에 색칠이 되는 스마트 유리창
+http://www.technologyreview.com/news/539946/smart-windows-just-got-a-lot-cooler/?utm_campaign=socialsync&utm_medium=social-post&utm_source=facebook
+
+13. 토스터 형태의 스마트폰 살균기
+http://www.earlyadopter.co.kr/61730
+
+14. 스쿠터처럼 타고다니는 여행기방
+http://www.earlyadopter.co.kr/61712
+
+15. 책상으로변하는 여행가방
+http://www.earlyadopter.co.kr/61480
+
+16. 물속에 넣으면 물고기를 모이게해서 낚시를 쉽게 도와주는 기기
+http://www.earlyadopter.co.kr/61803
+
+17. 레이져로 날아다니는 모기를 겨추시키는 기기
+http://www.earlyadopter.co.kr/61606
+
+
+18. 집에서 제작하는 고사양의 천체망원경.
+http://www.iflscience.com/technology/you-can-now-build-your-very-own-powerful-telescope-home
+
+19. 반려동물과 원격으로 소통할수 있게 도와주는 기기.
+http://www.engadget.com/2015/07/27/petzi-treat-cam/
+
+20. 세계최초 전자잉크 교통 안내 표지판(시카고)
+http://www.engadget.com/2015/07/27/sydney-e-ink-traffic-signs/
+
+21. 집에 가서 PC를 켜지 않아도 사진과 영상을 바로 확인하고 편집도 가능하게해주는 기기.
+http://www.engadget.com/2015/08/07/gnarbox-video-editing-suite/
+
+22. 착용하면 얼굴인식 당하지 않아 사생활 보호가 가능한 안경.
+http://gizmodo.com/these-glasses-block-facial-recognition-technology-1722826081?utm_campaign=socialflow_gizmodo_facebook&utm_source=gizmodo_facebook&utm_medium=socialflow
+
+23. 신체의 온도를 측정 관리하는 이어플러그
+https://www.kickstarter.com/projects/1460153872/yono-fertility-friend-a-wearable-basal-thermometer?utm_source=Early+Bird&utm_campaign=12e87803bf-Early_Bird_for_Kickstarter8_3_2015&utm_medium=email&utm_term=0_b6eff9d06a-12e87803bf-137822113
+
+#kickstarter
+
+150810 <tech>
+세일러묵 드림.
