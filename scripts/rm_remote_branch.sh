4fe57542611a14f1715dbd1c2c5607c98d9c4b32
JI-HUN KIM <jihuun.k@gmail.com>
2015-08-22 12:23:11 +0900

    Add remove remote branch script
    Signed-off-by: JI-HUN KIM <jihuun.k@gmail.com>

---
+++ scripts/rm_remote_branch.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#git push origin --delete development-process/Intro
+#git push origin --delete dp/intro
+#git push origin --delete dp/intro-branch
+#git push origin --delete dp/process
+#git push origin --delete dp/process-2
+#git push origin --delete dp/process_2
+#git push origin --delete linux/README--translating
+#git push origin --delete minimizing-target-document
+#git push origin --delete update-firstpage
+
+git push upstream --delete add-mac-compatible 
+git push upstream --delete removing-w-option  
