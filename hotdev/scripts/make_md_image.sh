fb9df54e96ba5ab5ac746ac98b2383b96e95ee89
JI-HUN KIM <jihuun.k@gmail.com>
2015-12-30 08:52:16 +0900

    Insert images to the contents
    150928 ~ 151228
    Signed-off-by: JI-HUN KIM <jihuun.k@gmail.com>

--- hotdev/scripts/make_md_image.sh
+++ hotdev/scripts/make_md_image.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 # Making link of image by markdown syntax
 # Using this script at the hotdev/image/.
+# Usage : image $ ../scripts/make_md_image.sh 16****
+# and next useful vim shortcut is "ty"
 
 ls $1/ > $1/list
 sed -i "s/^/![pic](\.\.\/image\/$1\//" ./$1/list
 sed -i 's/$/)/' ./$1/list
+
+vi -c :vs+:bn $1/list ../contents/$1.md
