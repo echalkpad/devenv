9ba4fdef97ceced4fabdbebde2b39c57ed4b5abe
JI-HUN KIM <jihuun.k@gmail.com>
2016-05-01 21:29:33 +0900

    update contents 160502
    Signed-off-by: JI-HUN KIM <jihuun.k@gmail.com>

--- hotdev/scripts/make_env.sh
+++ hotdev/scripts/make_env.sh
@@ -5,9 +5,10 @@ LINUX=Linux
 DIR="../contents"
 IMG="../image"
 
-cp $DIR/ref.md $DIR/$1.md
+cp -v $DIR/ref.md $DIR/$1.md
 mkdir $IMG/$1
-cp $IMG/backgound_img_maker_keynote.key $IMG/$1/
+cp -v $IMG/backgound_img_maker_keynote.key $IMG/$1/
+cp -v $IMG/format_3days.key $IMG/$1/
 
 if [[ $WHICH_SYSTEM == $MAC ]]; then
 	sed -i "s/150/$1/g" $DIR/$1.md
